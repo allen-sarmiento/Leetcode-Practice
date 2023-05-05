10ae2e58e7fb80216624e88139d7666aa1db802b
Allen Sarmiento <allenbsarmiento01@gmail.com>
2023-05-05 18:21:46 -0400

    Palindrome Number

---
+++ Palindrome Number/Solution.h
@@ -0,0 +1,19 @@
+using namespace std;
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0)
+            return false;
+        int temp = x;
+        unsigned long long rev = 0; 
+        unsigned int digit;
+        while (x > 0) {
+            digit = x%10;
+            x/=10;
+            rev = rev*10 + digit;
+        }
+        if (rev == temp)
+            return true;
+        return false;
+    }
+};
