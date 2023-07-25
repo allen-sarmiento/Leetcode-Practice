880e86042a20ad6bae2b2b3adf009072f035829a
Allen Sarmiento <allenbsarmiento01@gmail.com>
2023-07-25 19:47:40 -0400

    3sum

--- 15 - 3sum/Solution.h
+++ 15 - 3sum/Solution.h
@@ -1,51 +1,39 @@
+/*
+    Date: 7/26/2023
+    Time Complexity: O(n^2)
+    Space Complexity: O(1)
+*/
+
 #include <vector>
 #include <algorithm>
-#include <unordered_set>
-#include <set>
-
-#pragma once
-
-using namespace std;
 
 class Solution {
+
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-
-        /*
-            Triplets must have either:
-            - 1 Zero, and both positives and negatives of a number [-n,0,n]
-            - 2 Positives and 1 negative that are equal in magnitude [-(m+n), m, n]
-            - 2 negatives and 1 positive that are equal in magnitude [-m, -n, -(m+n)]
-            - 3 Zeroes [0,0,0]
-
-            i.e.
-
-            * A triplet must have three numbers [a,b,c] where a+b = -c
-            * Must have no duplicate triplet
-        */
-
-        // Stores all triplets
         vector<vector<int>> result;
+        sort(nums.begin(), nums.end());     // O(nlog(n))
 
-        // Sort the input array in descending order
-        sort(nums.rbegin(), nums.rend());
+        int lastI = INT_MIN;
+        for (int i = 0; i < nums.size(); i++) {     // O(n)
+            if (nums[i] == lastI) continue;
 
-        int lastC = NULL;
-        for (int c = 0; c < nums.size(); c++) {
-            int a = 1, b = nums.size()-1;
-            while(a < b) {
-                int sum = nums[a] + nums[b];
-                if (sum == -nums[0] && nums[0] != lastC) {
-                    result.push_back({nums[a], nums[b], nums[0]});
-                    lastC = nums[0];
-                    a++;
-                } 
-                else if (sum < -nums[0]) b--;
-                else a++;
+            int lp = i+1;
+            int rp = nums.size()-1;
+            while (lp < rp) {                       // O(n)
+                int sum = nums[i] + nums[lp] + nums[rp];
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[lp], nums[rp]});
+                    lp++;
+                    rp--;
+                    while (lp < rp && nums[lp] == nums[lp-1])
+                        lp++;   // Ensure that lp is unique
+                    while (lp < rp && nums[rp] == nums[rp+1])
+                        rp--;   // Ensure that rp is unique
+                } else if (sum < 0) lp++;
+                else rp--;
             }
-            // Swap so that c is always at index 0
-            swap(nums[0], nums[c]);
-            lastC = nums[0];
+            lastI = nums[i];
         }
         return result;
     }
--- 15 - 3sum/main.cpp
+++ 15 - 3sum/main.cpp
@@ -1,11 +1,22 @@
 #include "Solution.h"
 #include <vector>
+#include <iostream>
 
 using namespace std;
+
+void printVectorOfVectors(const vector<vector<int>>& vec) {
+    for (const auto& innerVec : vec) {
+        for (int num : innerVec) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+}
 
 int main() {
     Solution sol;
     vector<int> nums = {-1,0,1,2,-1,-4};
-    sol.threeSum(nums);
+    vector<vector<int>> result = sol.threeSum(nums);
+    printVectorOfVectors(result);
     return 0;
 }
